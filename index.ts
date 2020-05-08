57c76163616b8ce71c3324706d5431440ac14d6b
Bruna Garcia <garciabrunap@gmail.com>
2020-05-07 23:31:05 -0700

    Add promise.all and reduce request time

--- index.ts
+++ index.ts
@@ -1,6 +1,33 @@
 import fetch, { Response } from 'node-fetch'
 
-const charactersURL = "https://pokeapi.co/api/v2/evolution-chain/?limit=1"
+
+//Add Promise.all to getPokemonArray
+//Add type for getEvolvesTo - Create an interface 
+//Test getEvolvesTo
+
+interface PokemonDetails {
+  name: string;
+  pokemonType: string[];
+  evolvesTo: string[];
+}
+
+interface RawPokemon {
+  baby_trigger_item: null,
+  chain: {
+    evolution_details: any[],
+    evolves_to: any[][],
+    is_baby: boolean,
+    species: Species
+  },
+  id: number
+}
+
+interface Species {
+  name: string,
+  url: string
+}
+
+const charactersURL = "https://pokeapi.co/api/v2/evolution-chain/?limit=3"
 
 // fetch(charactersURL)
 //   .then(res => res.json())
@@ -18,11 +45,12 @@ const fetchPokemonAPI = async (url: string) => {
   }
 }
 
-const getPokemonArray = async (baseUrl: string) => {
+const getPokemonArray = async (baseUrl: string) : Promise<RawPokemon[]> => {
   const { results: urls } = await fetchPokemonAPI(baseUrl)
   const pokemonArray = []
   for (let pokemon of urls) {
-    const pokemonObject = await fetchPokemonAPI(pokemon.url)
+    const pokemonObject = (await fetchPokemonAPI(pokemon.url)) as RawPokemon
+    console.log(pokemonObject)
     pokemonArray.push(pokemonObject)
   }
   // console.log(pokemonArray)
@@ -41,24 +69,40 @@ const getPokemonSpecies = async (speciesUrl: string) => {
 
 //getEvolvesTo
 const getEvolvesTo = (evolvesArr: Array<any>) => {
-  return evolvesArr.map(({ species }) => species.name)
-  //Modify 
+  return evolvesArr.map(({ species }) => species.name) 
+}
+
+// the colan after the type will always mean that this function should return the specific value. 
+const getPokemonDetails = async (pokemon: any) : Promise<PokemonDetails> => {
+
+  const details = {
+    name: pokemon.chain.species.name,
+    pokemonType: await getPokemonSpecies(pokemon.chain.species.url),
+    evolvesTo: getEvolvesTo(pokemon.chain.evolves_to)
+  }
+  //returns array of promises 
+  return details 
 }
 
 //mainFunction - Printar todos os pokemons bonitinhos da array
 const main = async (url: string) => {
-  const arr = await getPokemonArray(url)
-  const mainArray = []
+  const pokemonList = await getPokemonArray(url)
+  console.time("request time")
+    
 
-  for (let pokemon of arr) {
-    let tempObj = {
-      'Name': pokemon.chain.species.name,
-      'Type': await getPokemonSpecies(pokemon.chain.species.url),
-      'EvolvesTo': getEvolvesTo(pokemon.chain.evolves_to)
-    }
-    console.log(tempObj)
-    mainArray.push(tempObj)
-  }
+    // const promises = pokemonList.map(pokemon => getPokemonDetails(pokemon))
+    const promises = pokemonList.map(getPokemonDetails)
+
+    //Using the array of promises as parameter for the promise.all 
+    const pokemonsDetails = await Promise.all(promises)
+    console.log(pokemonsDetails)
+
+  // for (let pokemon of pokemonList) {
+  //   const pokemonDetails = await getPokemonDetails(pokemon)
+  //   console.log(pokemonDetails)
+  //   mainArray.push(pokemonDetails)
+  // }
+  console.timeEnd("request time")
 }
 
 main(charactersURL)
